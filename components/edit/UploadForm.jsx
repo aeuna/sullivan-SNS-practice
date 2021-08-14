a498774e289cba090ff6bfa073eb574b66c6a5c1
aeuna <4294psy@naver.com>
2021-08-14 11:22:28 +0900

    수업 4차시 코드베이스 완료

--- components/edit/UploadForm.jsx
+++ components/edit/UploadForm.jsx
@@ -144,7 +144,7 @@ const UploadForm = (/*  알맞은 props를 전달해주세요. */) => {
   }
 
   async function updateFeed() {
-    /* 1. 피드를 업데이트 하기 위해, api에 전달해야하는 인자를 적어주세요 (4가지) */
+    /* 1. 피드를 업데이트 하기 위해, api에 전달해야하는 인자를 적어주세요 (5가지) */
     const updateParams = {
       /* 채워주세요. */
       /* 채워주세요. */
