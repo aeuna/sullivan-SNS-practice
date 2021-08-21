f82f66e75ae9d3611d25e9be5c68ed44938423cc
aeuna <4294psy@naver.com>
2021-08-21 13:58:49 +0900

    수업 4차시 추가과제 솔루션 수정

--- components/myfeed/Profile.jsx
+++ components/myfeed/Profile.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import { Grid, Typography } from "@material-ui/core";
 import { makeStyles } from "@material-ui/core/styles";
 import Avatar from "../common/Avatar";
-import ProfileUpdatePopup from "../myfeed/ProfileUpdatePopup";
+import ProfileUpdatePopup from "./ProfileUpdatePopup";
 
 const useStyles = makeStyles(() => ({
   profile: {
